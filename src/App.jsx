3e6f5a526466d4924da3adfad16cf3a14e3c27b4
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-03-10 11:13:03 +0100

    add time component and remove gh-pages dependency

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./style.css";
 import Header from "./Components/Heading";
 import GoogleSearch from "./Components/GoogleSearch";
 import Footer from "./Components/Footer";
+import Time from "./Components/Time";
 
 export const App = () => {
   return (
     <div className="App">
       <Header />
+      <Time />
       <GoogleSearch />
       <Footer />
     </div>
--- src/Components/Time.jsx
+++ src/Components/Time.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Time() {
+  //   const [time, setTime] = useState({ hour, minutes, seconds });
+  const [hour, setHour] = useState(Number);
+  const [minutes, setMinutes] = useState(Number);
+  const [seconds, setSeconds] = useState(Number);
+  const [suffix, setSuffix] = useState("");
+
+  useEffect(() => {
+    // run the getCurrentDate function every second
+    setInterval(getCurrentDate, 1000);
+
+    return clearInterval(getCurrentDate);
+  }, []);
+
+  function getCurrentDate() {
+    const date = new Date();
+
+    const [newHour, newMinutes, newSeconds] = [
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+    ];
+
+    setHour(newHour);
+    setMinutes(newMinutes);
+    setSeconds(newSeconds);
+    // setTime((prevTime) => ({
+    //   ...prevTime, // copy all other field/objects
+    //   hour: newHour,
+    //   minutes: newMinutes,
+    //   seconds: newSeconds,
+    // }));
+
+    // add suffix AM or PM
+    const suffixAmPm = `${newHour < 12 ? "AM" : "PM"}`;
+    setSuffix(suffixAmPm);
+
+    // display the time in the html
+    // hourDiv.textContent = hour;
+    // minuteDiv.textContent = `${minutes} `;
+    // secondsDiv.textContent = `${seconds} ${suffixAmPm}`;
+    // console.log(hour, minutes, seconds);
+    console.log(suffixAmPm);
+    console.log(newHour, newMinutes, newSeconds);
+    return newSeconds;
+  }
+
   return (
-    <div>Time</div>
-  )
+    <>
+      <div className="clock">
+        <div className="hour">{hour}</div>
+        <span>:</span>
+        <div className="minute">{`${minutes} `}</div>
+        <span>:</span>
+        <div className="seconds">{`${seconds} ${suffix}`}</div>
+      </div>
+    </>
+  );
 }
 
-export default Time
+export default Time;
